022c060ed4adb02885d6919b157b7e4822a582d8
Carlos Lugo <cl12corp@icloud.com>
2017-03-29 19:12:30 -0700

    Carlos Lugo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title','Bienvenido')
+@section('content')
+<div class="container">
+  <div class="jumbotron">
+    <h1>Bootstrap Tutorial</h1> 
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam vel dolore eos voluptas non alias molestias explicabo libero eveniet est aperiam, quasi iure natus, incidunt iusto tempore consectetur, ea magnam!</p> 
+   <p> <a href="#" class= "bnt btn-primary"> Crea tu Blog </a> 
+  </div>
+@endsection
+  
